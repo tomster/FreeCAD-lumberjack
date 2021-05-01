d16f9a2eecc313efe92f54b506b50bb52354ff37
Tom Lazar <tom.lazar@zeit.de>
2021-05-01 11:42:53 +0200

    Print CSV for cutlistoptimizer

--- freecad/lumberjack/cutlist.py
+++ freecad/lumberjack/cutlist.py
@@ -21,7 +21,25 @@ def get_parts_for_selection():
     return parts
 
 
+def get_dimensions(parts):
+    return [(obj, sorted(utils.dimensions_of_obj(obj), reverse=True)) for obj in parts]
+
+
 def main():
-    parts = get_parts_for_selection()
-    for part in parts:
-        print("Got: %s" % part.Label)
+    dims = get_dimensions(get_parts_for_selection())
+    print_cutlist_csv(dims)
+    return dims
+
+
+def print_cutlist_csv(dimensions):
+    print("""Length,Width,Qty,Material,Label,Enabled""")
+    for part, dimension in dimensions:
+        print(
+            "%d, %d, 1, %d, %s, true"
+            % (
+                dimension[0],
+                dimension[1],
+                dimension[2],
+                part.Label,
+            )
+        )
