d54b12e63d82026caa7f4ba4063ef0fe02d02fd4
Tom Lazar <tom.lazar@zeit.de>
2021-05-03 08:53:02 +0200

    Factor into class

--- freecad/lumberjack/cutlist.py
+++ freecad/lumberjack/cutlist.py
@@ -9,43 +9,12 @@ def reload():
     _reload(this)
 
 
-def get_parts_for_selection():
-    selection = Gui.Selection.getSelection()
-
-    parts = []
-
-    for item in selection:
-        print("selection: %s" % item.Label)
-        parts.extend(utils.get_part(item))
-
-    return parts
-
-
-def get_dimensions(parts):
-    return [(obj, sorted(utils.dimensions_of_obj(obj), reverse=True)) for obj in parts]
-
-
 def main():
-    dims = get_dimensions(get_parts_for_selection())
-    print_cutlist_csv(dims)
-    return dims
+    list = CutList(Gui.Selection.getSelection())
+    return list
 
-
-def print_cutlist_csv(dimensions):
-    print("""Length,Width,Qty,Material,Label,Enabled""")
-    for part, dimension in dimensions:
-        print(
-            "%d, %d, 1, %d,%s, true"
-            % (
-                dimension[0],
-                dimension[1],
-                dimension[2],
-                part.Label,
-            )
-        )
 
 class CutList:
-
     def __init__(self, items):
 
         self.parts = []
@@ -56,3 +25,25 @@ class CutList:
             names.append(item.Label)
 
         self.name = "%sCutlist" % "".join(["%s-" % name for name in names])
+
+    def get_dimensions(self):
+        return [
+            (obj, sorted(utils.dimensions_of_obj(obj), reverse=True))
+            for obj in self.parts
+        ]
+
+    def print_cutlist_csv(self):
+        print("""Length,Width,Qty,Material,Label,Enabled""")
+        for part, dimension in self.get_dimensions():
+            print(
+                "%d, %d, 1, %d,%s, true"
+                % (
+                    dimension[0],
+                    dimension[1],
+                    dimension[2],
+                    part.Label,
+                )
+            )
+
+    def __repr__(self):
+        print("<%>: %s" % (self.__class__, self.name))
