0af442cfbf2d44ab80f161051d236df33254ea40
Tom Lazar <tom.lazar@zeit.de>
2021-12-04 18:54:26 +0100

    Provide some more examples

--- freecad/lumberjack/sandbox.py
+++ freecad/lumberjack/sandbox.py
@@ -2,3 +2,11 @@
 from freecad.lumberjack import cutlist as cl
 from freecad.lumberjack import utils
 from importlib import reload
+
+# then you can select something in the GUI and do
+# this
+foo = cl.main()
+foo.write_cutlist_csv()
+
+# after you make changes in code you can then
+reload(cl)
